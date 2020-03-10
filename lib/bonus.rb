552ac48f261e5ee0b4ddcd8bc204ed0e4fea1c7e
Sajid Anjum <sajid1760@gmail.com>
2020-03-10 21:25:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,5 +28,5 @@ epic_tragedy[:caputlet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+ puts epic_tragedy
 end
